4df9af50c68198111b36ab000b60b1d968aa050f
NickLakman <140800149+NickLakman@users.noreply.github.com>
2023-11-13 15:45:10 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,53 +1,13 @@
 
     for item in data:
-def main():
 
     return data
-
-    data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
     main()
-import random
-        print(f"Random Number: {item}")
-
-if __name__ == "__main__":
-def generate_random_data():    main()
     data = [random.randint(1, 100) for _ in range(10)]
 import random
-    data = generate_random_data()
 if __name__ == "__main__":
         print(f"Random Number: {item}")
-
-    for item in data:
-    return data
-
-
+    data = generate_random_data()
 def generate_random_data():
-
-def main():        print(f"Random Number: {item}")
-
-def generate_random_data():
-if __name__ == "__main__":
 
 def main():
-
-
-    main()
-    return data
-    data = generate_random_data()
-    for item in data:
-    data = [random.randint(1, 100) for _ in range(10)]
-import random    main()
-def generate_random_data():
-    for item in data:
-import random
-if __name__ == "__main__":
-
-
-def main():
-
-        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data
-
-    data = generate_random_data()
